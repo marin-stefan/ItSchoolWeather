89276464c5867fe7d4be1517324c92ded1bfe40d
stefann06 <stefann06@yahoo.com>
2025-11-12 13:19:19 +0200

    more formatting,refactor, added material icon and functionality and css for scroll to top icon,

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // ne selectam tagul de html care contine orasul curent si impreuna cu cheia
 // din local storage o sa setam ce folosim
-const currentCityTag = document.querySelector('.current-city');
-let currentCityFromLocalStorage = localStorage.getItem('city');
+const currentCityTag = document.querySelector(".current-city");
+let currentCityFromLocalStorage = localStorage.getItem("city");
+
+// selectam icon de scroll to top
+const scrollToTopIcon = document.querySelector(".scroll-to-top");
 
 //daca nu avem nimic in cheia city in locastorage  atunci salvam ca default Bucuresti
 if (!currentCityFromLocalStorage) {
-    localStorage.setItem('city', 'București');
-    currentCityFromLocalStorage = 'București'
-};
+    localStorage.setItem("city", "București");
+    currentCityFromLocalStorage = "București";
+}
 
 //actualizam pe ecran numele orasului
 currentCityTag.innerHTML = currentCityFromLocalStorage;
@@ -17,3 +20,20 @@ displayCurrentWeather(currentCityFromLocalStorage);
 
 // afisam vremea pe urm 5 zile
 displayWeatherForecast(currentCityFromLocalStorage);
+
+// adaugam eventListener de click pe icon de scroll to top
+scrollToTopIcon.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+});
+
+// adaugam eventListener pe document pt a ascunde/arata butonul de scroll to top 
+document.addEventListener("scroll", () => {
+    // aratam icon de scroll cand scrollul este mai mare de jumatatea viewPortului
+    // viewport-ul difera in functie de rzolutia actuala a user-ului
+    scrollToTopIcon.style.visibility = window.scrollY > window.innerHeight / 2 
+        ? "visible"
+        : "hidden"
+});
